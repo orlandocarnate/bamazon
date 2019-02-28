826e874eb4d1e75899601e8d6b107abe27eed739
Orlando Carnate <orlando.carnate@gmail.com>
2019-02-27 20:36:20 -0600

    [bamazonManager.js] got app to list all products.

--- bamazonManager.js
+++ bamazonManager.js
@@ -18,18 +18,11 @@ const closeDB = () => {
     connection.end();
 }
 
-/**
- * Heirarchical conversation example
- */
-
-'use strict';
-var inquirer = require('..');
-
 var menu = {
     type: 'list',
     name: 'menu',
     message: 'What would you like to do?',
-    choices: ['View Products For Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
+    choices: ['View Products For Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'Quit']
 };
 
 function start() {
@@ -37,20 +30,73 @@ function start() {
     menu();
 }
 
-function menu() {
+const exitApp = () => {
+    inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'gohome',
+            message: 'Are you sure you want to Quit?',
+            default: false
+        }
+    ]).then(answers => {
+        if (answers.gohome) {
+            console.log("Goodbye!");
+            closeDB();
+        } else {
+            mainMenu();
+        }
+    })
+}
+
+function viewProducts(callback) {
+    const queryStr = "SELECT * FROM products";
+    connection.query(queryStr, function (err, response) {
+        if (err) throw err;
+        // callback(); // CALLBACK
+        products = response; // assign to global variable
+        callback(response);
+    });
+}
+
+function showProducts(arg) {
+    console.log(arg.map(element => {
+        return `ID: ${element.item_id} Name: ${element.product_name} Price: $${element.price} Qty Left: ${element.stock_quantity}`
+    }).join("\n"));
+    goHome();
+}
+
+function viewLowInventory() {
+    // show inventory where QTY <= 5
+}
+
+function addToInventory() {
+    // increase QTY of specific product
+}
+
+function addNewProduct() {
+    // INSERT INTO a new product.
+}
+
+function mainMenu() {
+    console.log('\033[2J'); // clears screen
     inquirer.prompt(menu).then(answers => {
-        switch (answers) {
-            case answers.name === 'View Products For Sale':
-                viewProducts(goHome)
+        // console.log(answers);
+        switch (answers.menu) {
+
+            case 'View Products For Sale':
+                viewProducts(showProducts)
                 break;
-            case answers.name === 'View Low Inventory':
+            case 'View Low Inventory':
                 viewLowInventory(goHome)
                 break;
-            case answers.name === 'Add to Inventory':
+            case 'Add to Inventory':
                 addToInventory(goHome)
                 break;
-            case answers.name === 'Add New Product':
+            case 'Add New Product':
                 addNewProduct(goHome)
+                break;
+            case 'Quit':
+                exitApp();
                 break;
         }
     });
@@ -62,73 +108,16 @@ function goHome() {
             type: 'confirm',
             name: 'gohome',
             message: 'Return to Menu?',
-            default: 
+            default: true
         }
     ]).then(answers => {
         if (answers.gohome) {
-            menu();
+            mainMenu();
         } else {
             console.log("Goodbye!");
+            closeDB();
         }
     })
 }
 
-function viewProducts() {
-
-}
-
-function viewLowInventory() {
-    inquirer.prompt(directionsPrompt).then(answers => {
-        var direction = answers.direction;
-        if (direction === 'Forward') {
-            var output = 'You find a painted wooden sign that says:';
-            output += ' \n';
-            output += ' ____  _____  ____  _____ \n';
-            output += '(_  _)(  _  )(  _ \\(  _  ) \n';
-            output += '  )(   )(_)(  )(_) ))(_)(  \n';
-            output += ' (__) (_____)(____/(_____) \n';
-            console.log(output);
-        } else {
-            console.log('You cannot go that way');
-            encounter2a();
-        }
-    });
-}
-
-function addToInventory() {
-    inquirer
-        .prompt({
-            type: 'list',
-            name: 'weapon',
-            message: 'Pick one',
-            choices: [
-                'Use the stick',
-                'Grab a large rock',
-                'Try and make a run for it',
-                'Attack the wolf unarmed'
-            ]
-        })
-        .then(() => {
-            console.log('The wolf mauls you. You die. The end.');
-        });
-}
-
-function addNewProduct() {
-    inquirer
-        .prompt({
-            type: 'list',
-            name: 'weapon',
-            message: 'Pick one',
-            choices: [
-                'Use the stick',
-                'Grab a large rock',
-                'Try and make a run for it',
-                'Attack the wolf unarmed'
-            ]
-        })
-        .then(() => {
-            console.log('The wolf mauls you. You die. The end.');
-        });
-}
-
-menu();
+mainMenu();
